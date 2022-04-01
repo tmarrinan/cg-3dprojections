7091b7f3b44f39b7a0635165c47988a87c26f4bb
xion6136 <71159252+xion6136@users.noreply.github.com>
2022-04-01 13:21:44 -0500

    Completed Clipping

--- scripts/renderscene.js
+++ scripts/renderscene.js
@@ -75,6 +75,7 @@ function init() {
     let testClip = [-12,6,-12,6,10,100];
     let res = mat4x4Perspective(testPRP, testSRP, testVUP, testClip);
  */
+    console.log(16 & RIGHT);
 
 }
 
@@ -186,7 +187,7 @@ function clipLinePerspective(line, z_min) {
             // Just break and do nothing since result was already initialize to null
             break;
         } else {
-            // BOUNDS: LEFT = Z, RIGHT = -Z, BOTTOM =
+            // BOUNDS: LEFT = z, RIGHT = -z, BOTTOM y = z, TOP = -z, FAR z = 1, NEAR z = z_min
             let outcode = null;
             let x,y,z = null;
             let t = null;
@@ -198,42 +199,68 @@ function clipLinePerspective(line, z_min) {
                 outcode = out1;
             }
 
-            // TODO: find intersection point to set for new line.
             // Check via LEFT plane
             if(outcode & LEFT) {
                 t = (-p0.x + p0.z) / ((p1.x - p0.x) - (p1.z - p0.z));
-
+                x = ((1-t) * p0.x) + (t * p1.x);
+                y = ((1-t) * p0.y) + (t * p1.y);
+                z = ((1-t) * p0.z) + (t * p1.z);
             }
             // Check via RIGHT plane
             else if(outcode & RIGHT) {
                                     // Flipped points for negative change in slope
                 t = (p0.x + p0.z) / ((p0.x - p1.x) - (p0.z - p1.z));
-
+                x = ((1-t) * p0.x) + (t * p1.x);
+                y = ((1-t) * p0.y) + (t * p1.y);
+                z = ((1-t) * p0.z) + (t * p1.z);
             }
             // Check via BOTTOM plane
             else if(outcode & BOTTOM) {
                 t = (-p0.y + p0.z) / ((p1.y - p0.y) - (p1.z - p0.z));
-
+                x = ((1-t) * p0.x) + (t * p1.x);
+                y = ((1-t) * p0.y) + (t * p1.y);
+                z = ((1-t) * p0.z) + (t * p1.z);
             }
             // Check via TOP plane
             else if(outcode & TOP) {
                                     // Flipped points for negative change in slope
                 t = (p0.y + p0.z) / ((p0.y - p1.y) - (p0.z - p1.z));
-
+                x = ((1-t) * p0.x) + (t * p1.x);
+                y = ((1-t) * p0.y) + (t * p1.y);
+                z = ((1-t) * p0.z) + (t * p1.z);
             }
             // Check via FAR plane
             else if(outcode & FAR) {
                 // Flipped points for negative change in slope
                 t = (p0.z - z_min) / (p0.z- p1.z);
+                x = ((1-t) * p0.x) + (t * p1.x);
+                y = ((1-t) * p0.y) + (t * p1.y);
+                z = ((1-t) * p0.z) + (t * p1.z);
             }
             // Check via NEAR plane
             else {
                 t = (-p0.z - 1) / (p1.z- p0.z);
-
+                x = ((1-t) * p0.x) + (t * p1.x);
+                y = ((1-t) * p0.y) + (t * p1.y);
+                z = ((1-t) * p0.z) + (t * p1.z);
             }
 
+            if(outcode == out1) {
+                p0.x = x;
+                p0.y = y;
+                p0.z = z;
+                out0 = outcode;
+            } else {
+                p1.x = x;
+                p1.y = y;
+                p1.z = z;
+                out1 = outcode;
+            }
         }
     }
+    line.pt0 = p0;
+    line.pt1 = p1;
+    result = line;
 
     return result;
 }
